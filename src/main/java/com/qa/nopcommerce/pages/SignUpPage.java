5062665fa173950586cb2578731b49f296520e82
Sumit <sumit.shivhare29@gmail.com>
2021-01-17 22:15:11 +0530

    created SignUpPage.java under pages

--- src/main/java/com/qa/nopcommerce/pages/SignUpPage.java
+++ src/main/java/com/qa/nopcommerce/pages/SignUpPage.java
@@ -1,5 +1,25 @@
 package com.qa.nopcommerce.pages;
 
-public class SignUpPage {
+import org.openqa.selenium.WebDriver;
 
+import com.qa.nopcommerce.utils.ElementUtil;
+
+public class SignUpPage {
+	
+	public WebDriver driver;
+	public ElementUtil util;
+	
+	//By locators
+	
+	
+
+	//2. constructor
+	public SignUpPage(WebDriver driver) {
+		this.driver = driver;
+		util = new ElementUtil(this.driver);
+		
+		}
+
+	
+	//Page actions
 }
