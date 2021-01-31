cd683a99eb918967fdd17be09e461afdb7a71299
Sumit <sumit.shivhare29@gmail.com>
2021-02-01 01:26:52 +0530

    modified element utils

--- src/main/java/com/qa/nopcommerce/utils/ElementUtil.java
+++ src/main/java/com/qa/nopcommerce/utils/ElementUtil.java
@@ -171,9 +171,9 @@ public class ElementUtil {
 		return wait.until(ExpectedConditions.visibilityOfAllElementsLocatedBy(locator));
 	}
 
-	public void getPageLinksText(By locator, int timeOut) {
-		visibilityOfAllElements(locator, timeOut).stream().forEach(ele -> System.out.println(ele.getText()));
-	}
+//	public void getPageLinksText(By locator, int timeOut) {
+//		visibilityOfAllElements(locator, timeOut).stream().forEach(ele -> System.out.println(ele.getText()));
+//	}
 
 	public int getPageLinksCount(By locator, int timeOut) {
 		return visibilityOfAllElements(locator, timeOut).size();
